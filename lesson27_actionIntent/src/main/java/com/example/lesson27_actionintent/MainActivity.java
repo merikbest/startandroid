486df79c298b342fccb3e510ae81db1861325e92
merik <merikbest2015@gmail.com>
2020-09-05 22:08:37 +0300

    done

---
+++ lesson27_actionIntent/src/main/java/com/example/lesson27_actionintent/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lesson27_actionintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnTime = findViewById(R.id.btnTime);
+        Button btnDate = findViewById(R.id.btnDate);
+
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+
+        switch (v.getId()) {
+            case R.id.btnTime:
+                intent = new Intent("ru.startandroid.intent.action.showtime");
+                startActivity(intent);
+                break;
+            case R.id.btnDate:
+                intent = new Intent("ru.startandroid.intent.action.showdate");
+                startActivity(intent);
+                break;
+        }
+    }
+}
---
+++ lesson28_extras/src/main/java/com/example/lesson28_extras/ViewActivity.java
@@ -0,0 +1,26 @@
+package com.example.lesson28_extras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ViewActivity extends AppCompatActivity {
+    TextView tvView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        tvView = (TextView) findViewById(R.id.tvView);
+
+        Intent intent = getIntent();
+
+        String fName = intent.getStringExtra("fname");
+        String lName = intent.getStringExtra("lname");
+
+        tvView.setText("Your name is: " + fName + " " + lName);
+    }
+}
